e52ca8376483bda14368c7de2a1af50d9e8cb43c
exynos6694 <azsx6694@naver.com>
2025-06-28 00:42:47 +0900

    불필요 라이브러리 제거

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import '../style/App.css'
 import Navbar from './Navbar';
 import Section from './Section';
